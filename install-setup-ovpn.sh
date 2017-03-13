641842f6844f53a5499f51b6093bf4d6e7c4c7d2
Stephen Hickie <stephen.hickie@smsmt.com>
2017-03-13 16:50:10 +1100

    add install checker

--- install-setup-ovpn.sh
+++ install-setup-ovpn.sh
@@ -1,4 +1,6 @@
 sudo su - ## make me root
+if [ -f ~/client1.ovpn ] ; then logger -t install "already installed SMS" && exit ; else logger -t install "install openvpn SMS" ; fi
+
 apt-get install openvpn easy-rsa -y
 gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server.conf
 sed -i "s/dh1024/dh2048/g" /etc/openvpn/server.conf
@@ -11,6 +13,7 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 sed -i "s/#net.ipv4.ip_forward/net.ipv4.ip_forward/g" /etc/sysctl.conf
 ufw allow https
 sed -i "s/DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/g" /etc/default/ufw
+
 cp -r /usr/share/easy-rsa/ /etc/openvpn
 cd /etc/openvpn/easy-rsa
 source vars
@@ -20,7 +23,22 @@ echo 490B82C4000000000075 > /etc/openvpn/easy-rsa/keys/serial
 ./pkitool --server server
 ./pkitool client1
 cp /etc/openvpn/easy-rsa/keys/ca* /etc/openvpn/
-cp /etc/openvpn/easy-rsa/keys/se* /etc/openvpn/
+cp /etc/openvpn/easy-rsa/keys/server.* /etc/openvpn/
+
+mv /etc/ufw/before.rules ~/
+head ~/before.rules -n 10 >> /etc/ufw/before.rules
+cat <<EOF >> /etc/ufw/before.rules
+# START OPENVPN RULES
+# NAT table rules
+*nat
+:POSTROUTING ACCEPT [0:0]
+# Allow traffic from OpenVPN client to eth0
+-A POSTROUTING -s 10.8.0.0/8 -o eth0 -j MASQUERADE
+COMMIT
+# END OPENVPN RULES
+EOF
+tail ~/before.rules -n 68 >> /etc/ufw/before.rules
+
 ## all thing being equal we should now be able to start the server
 server openvpn start
 cat <<EOF > ~/client1.ovpn
